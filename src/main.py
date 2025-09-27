2bdf606abb1fba53ed21ca8618dce993a920de35
ViniciusVViana <viniciusvianav1099@gmail.com>
2025-09-27 18:34:34 -0300

    Remoção do código que foi mandado para a pasta de utils

--- src/main.py
+++ src/main.py
@@ -1,63 +1,6 @@
 import sys
-import re
 
-patterns_list = [
-
-    # Comentários
-    (r"saturnita.*", "COMENTARIO"),
-
-    # Quebra de linha e espaços
-    (r"\n", "QUEBRA DE LINHA"),
-    (r"\t", "TABULACAO"),
-    (r" ", "ESPACO"),
-
-    # Palavras-chave
-    (r"tralelero|tralala|porcodio|porcoala", "TIPO DE VARIAVEL"),
-    (r"lirili|larila", "INICIO E FIM DE ESTRUTURA DE DECISAO"),
-    (r"dunmadin", "INICIO DE LACO CONTADO"),
-    (r"tung|sahur", "INICIO E FIM DE LACO DE REPETICAO"),
-    (r"chimpanzini", "COMANDO DE SAIDA"),
-    (r"batapim", "COMANDO DE ENTRADA"),
-    (r"delimitare|finitini", "DELIMITADOR DE BLOCO"),
-    (r"tripi|tropa", "VALOR BOOLEANO"),
-
-    # Identificadores (após palavras-chave)
-    (r"[a-z][a-zA-Z0-9]*", "IDENTIFICADOR"),
-
-    # Literais
-    (r"\d+\.\d+", "NUMERO REAL"),
-    (r"\d+", "NUMERO INTEIRO"),
-    (r"\".*?\"", "STRING"),
-    (r"'.*?'", "CARACTERE"),
-
-    # Operadores
-    (r"\+|\-|\*|\/|\%", "OPERADOR ARITMETICO"),
-    (r"==|!=|>=|<=|>|<", "OPERADOR RELACIONAL"),
-    (r"\&\&|\|\|", "OPERADOR LOGICO"),
-    (r"=", "ATRIBUICAO"),
-    (r";", "FIM DE INSTRUCAO"),
-
-    # Captura qualquer outro caractere (erro léxico)
-    (r".", "TOKEN INVALIDO"),
-]
-
-
-# ========== Análise Léxica ==========
-# Função que realiza a análise léxica do arquivo de entrada
-def lex_anal(arch):
-    tokens = []
-    line = 1
-    while arch:
-        for pattern, desc in patterns_list:
-            match = re.match(pattern, arch)
-            if match:
-                tokens.append((match.group(0), desc, line))
-                if tokens[-1][0] == "\n":
-                    line += 1
-                arch = arch[match.end():]
-                break
-            
-    return tokens
+from utils import lex_anal
 
 def main():
     if len(sys.argv) > 1:
